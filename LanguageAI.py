33446d45f0de8f7d4dbdd294f8c4d5fa2daa623d
MrBofus <drewruss3@gmail.com>
2021-12-29 00:03:54 -0600

    Added LanguageAI.py and some text documents.

---
+++ LanguageAI.py
@@ -0,0 +1,262 @@
+################################################################################
+################################################################################
+
+import pandas as pd
+from spacy.lang.en import English
+import torch
+from torch.utils.data import Dataset, DataLoader
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, AdamW, get_linear_schedule_with_warmup
+from tqdm import tqdm, trange
+import torch.nn.functional as F
+import os
+
+################################################################################
+################################################################################
+
+TRAIN_ON_RUN = 0
+
+max_length = 1024
+temp = 0.95
+top_k = 50
+
+################################################################################
+################################################################################
+
+print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+print('\n\n==================================================================================================================================')
+print('==================================================================================================================================\n\n')
+print('\t\t\t\t\t\t\tHORROR-BOT\n')
+print('\t\t\t\ta whacky and uncharacteristic tale in a few short clicks\n')
+print('\n\n==================================================================================================================================')
+print('==================================================================================================================================\n\n')
+print('\tTo begin, fill in some starting seeds. These sentence fragments will be used to generate a story.')
+print('\tTry something like \"It was a dark and stormy night when suddenly, \"')
+print('\tThe sentence fragment will then be auto-completed using an advanced AI. The longer the starting \n\tseeds, the more \'realistic\' the story will sound.')
+prompt1 = input('\n\nInput starting seed 1: ')
+prompt2 = input('\nInput starting seed 2: ')
+prompt3 = input('\nInput starting seed 3: ')
+prompt4 = input('\nInput starting seed 4: ')
+print('\n\n==================================================================================================================================')
+print('==================================================================================================================================\n\n')
+print('\n\n\t\tProcessing...\n\n')
+
+################################################################################
+################################################################################
+
+class storydata(Dataset):  
+    def __init__(self, control_code, truncate=False, gpt2_type="gpt2", max_length=1024):
+
+        self.tokenizer = GPT2Tokenizer.from_pretrained(gpt2_type)
+        self.sentences = []
+
+        for row in sentences[0]:
+          self.sentences.append(torch.tensor(
+                self.tokenizer.encode(f"<|{control_code}|>{row[:max_length]}<|endoftext|>")
+            ))               
+        if truncate:
+            self.sentences = self.sentences[:20000]
+        self.sentences_count = len(self.sentences)
+        
+    def __len__(self):
+        return self.sentences_count
+
+    def __getitem__(self, item):
+        return self.sentences[item]
+
+def pack_tensor(new_tensor, packed_tensor, max_seq_len):
+    if packed_tensor is None:
+        return new_tensor, True, None
+    if new_tensor.size()[1] + packed_tensor.size()[1] > max_seq_len:
+        return packed_tensor, False, new_tensor
+    else:
+        packed_tensor = torch.cat([new_tensor, packed_tensor[:, 1:]], dim=1)
+        return packed_tensor, True, None
+
+def train(
+    dataset, model, tokenizer,
+    batch_size=16, epochs=5, lr=2e-5,
+    max_seq_len=400, warmup_steps=200,
+    gpt2_type="gpt2", output_dir=".", output_prefix="wreckgar",
+    test_mode=False,save_model_on_epoch=False,
+):
+    device=torch.device("cpu")
+    model = model.cpu()
+    model.train()
+
+    optimizer = AdamW(model.parameters(), lr=lr)
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer, num_warmup_steps=warmup_steps, num_training_steps=-1
+    )
+
+    train_dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+    loss=0
+    accumulating_batch_count = 0
+    input_tensor = None
+
+    for epoch in range(epochs):
+
+        print(f"Training epoch {epoch}")
+        print(loss)
+        for idx, entry in tqdm(enumerate(train_dataloader)):
+            (input_tensor, carry_on, remainder) = pack_tensor(entry, input_tensor, 768)
+
+            if carry_on and idx != len(train_dataloader) - 1:
+                continue
+
+            input_tensor = input_tensor.to(device)
+            outputs = model(input_tensor, labels=input_tensor)
+            loss = outputs[0]
+            loss.backward()
+
+            if (accumulating_batch_count % batch_size) == 0:
+                optimizer.step()
+                scheduler.step()
+                optimizer.zero_grad()
+                model.zero_grad()
+
+            accumulating_batch_count += 1
+            input_tensor = None
+        if save_model_on_epoch:
+            torch.save(
+                model.state_dict(),
+                os.path.join(output_dir, f"{output_prefix}-{epoch}.pt"),
+            )
+    return model
+
+
+def generate(
+    model,
+    tokenizer,
+    prompt,
+    entry_count=10,
+    entry_length=45, #maximum number of words
+    top_p=0.8,
+    temperature=temp,
+):
+
+    model.eval()
+
+    generated_num = 0
+    generated_list = []
+
+    filter_value = -float("Inf")
+
+    with torch.no_grad():
+
+        for entry_idx in trange(entry_count):
+
+            entry_finished = False
+
+            generated = torch.tensor(tokenizer.encode(prompt)).unsqueeze(0)
+
+            for i in range(entry_length):
+                outputs = model(generated, labels=generated)
+                loss, logits = outputs[:2]
+                logits = logits[:, -1, :] / (temperature if temperature > 0 else 1.0)
+
+                sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+                cumulative_probs = torch.cumsum(F.softmax(sorted_logits, dim=-1), dim=-1)
+
+                sorted_indices_to_remove = cumulative_probs > top_p
+                sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[
+                    ..., :-1
+                ].clone()
+                sorted_indices_to_remove[..., 0] = 0
+
+                indices_to_remove = sorted_indices[sorted_indices_to_remove]
+                logits[:, indices_to_remove] = filter_value
+
+                next_token = torch.multinomial(F.softmax(logits, dim=-1), num_samples=1)
+                generated = torch.cat((generated, next_token), dim=1)
+
+                if next_token in tokenizer.encode("<|endoftext|>"):
+                    entry_finished = True
+
+                if entry_finished:
+
+                    generated_num = generated_num + 1
+
+                    output_list = list(generated.squeeze().numpy())
+                    output_text = tokenizer.decode(output_list)
+                    generated_list.append(output_text)
+                    break
+            
+            if not entry_finished:
+              output_list = list(generated.squeeze().numpy())
+              output_text = f"{tokenizer.decode(output_list)}<|endoftext|>" 
+              generated_list.append(output_text)
+                
+    return generated_list
+
+def text_generation(test_data):
+  generated_lyrics = []
+  for i in range(len(test_data)):
+    x = generate(model.to('cpu'), tokenizer, test_data[0][i], entry_count=1)
+    generated_lyrics.append(x)
+  return generated_lyrics
+
+################################################################################
+################################################################################
+
+dracula = open("dracula.txt").read()
+    
+text_in = dracula
+text_in = text_in.replace('\n',' ')
+text_in = text_in.replace('\xa0','')
+    
+nlp = English()
+nlp.add_pipe('sentencizer')
+doc = nlp(text_in)
+sentences = [sent.text.strip() for sent in doc.sents]
+sentences = pd.DataFrame(sentences)
+sentences = sentences[sentences[0].apply(lambda x: len(x.split(' ')) < 350)]
+    
+dataset = storydata(sentences[0], truncate=True, gpt2_type="gpt2") 
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+
+if TRAIN_ON_RUN:   
+    model = GPT2LMHeadModel.from_pretrained('gpt2')
+    model = train(dataset, model, tokenizer)
+
+else:  
+    model = torch.load('model.pt')
+    
+################################################################################
+################################################################################
+
+start_str = pd.DataFrame({0:[prompt1,
+                             prompt2,
+                             prompt3,
+                             prompt4]})
+
+print('\t\tGenerating story...\n\n\n\n')
+generated_story = text_generation(start_str)
+
+i = 0
+generated_story_str = '\t'
+
+while i < len(generated_story):
+    generated_story_str = generated_story_str + ' ' + str(generated_story[i])
+    i += 1
+
+generated_story = generated_story_str
+
+generated_story = generated_story.replace('[\'', '')
+generated_story = generated_story.replace('[\"','')
+generated_story = generated_story.replace('\']', '')
+generated_story = generated_story.replace('\"]','\"')
+generated_story = generated_story.replace('\\xa0', '')
+generated_story = generated_story.replace('<|endoftext|>','.')
+generated_story = generated_story.replace('.,','.')
+generated_story = generated_story.replace(',.','.')
+generated_story = generated_story.replace('..','.')
+generated_story = generated_story.replace('      DRACULA     CHAPTER I  JONATHAN HARKERS JOURN','')
+generated_story = generated_story.replace('\\n\\n', ' ')
+generated_story = generated_story.replace('\\n','')
+generated_story = generated_story.replace('.\".', '.\"')
+generated_story = generated_story.replace('\\\'','\'')
+generated_story = generated_story.replace('\\\"','\"')
+
+print('\n\n==================================================================================================================================')
+print('==================================================================================================================================\n\n')      
+print(generated_story + '\n\n')
